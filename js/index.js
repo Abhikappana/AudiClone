460ca7abc63f7407319a3ab447c6f7c74bc69506
Abhikappana <abhikappana@gmail.com>
2023-10-08 11:12:41 +0530

    commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const carDetails =document.querySelectorAll(".cardetails");
+const imgCards =document.querySelectorAll(".img-card");
+const detailsCloseBtns =document.querySelectorAll(".details-close-btn");
+
+var cardetail = function(modalClick){
+    carDetails[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) =>{
+    imgCard.addEventListener("click",() =>{
+        cardetail(i);
+    });
+});
+
+detailCloseBtns.forEach((detailsCloseBtn) =>{
+    detailsCloseBtn.addEventListener("click",() =>{
+        carDetails.forEach((carDetailView) => {
+            carDetailView.classList.remove("active");
+        });
+    });
+});
